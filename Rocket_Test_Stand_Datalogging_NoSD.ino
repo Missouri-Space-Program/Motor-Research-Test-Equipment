91acd86603daf69487e46889e820bceda28770dc
Feistlyone <30285867+Feistlyone@users.noreply.github.com>
2020-01-24 18:37:51 -0600

    Update Rocket_Test_Stand_Datalogging_NoSD.ino

--- Rocket_Test_Stand_Datalogging_NoSD.ino
+++ Rocket_Test_Stand_Datalogging_NoSD.ino
@@ -8,15 +8,16 @@
 #define calibration_factor 8980 //This value is obtained using the SparkFun_HX711_Calibration sketch
 #define LOADCELL_DOUT_PIN  3
 #define LOADCELL_SCK_PIN  2
-#define DATAQ_pin 1 //arduino analog 1 is attached to channel on DATAQ and arduino gnd is connected
-float reading;
-float rawreading;
+#define DATAQ_pin 9 //arduino analog 1 is attached to channel on DATAQ and arduino gnd is connected
+int reading;
+int rawreading;
 HX711 scale;
 
 void setup()
 {
   // Open serial communications and wait for port to open:
-  Serial.begin(19200);
+  Serial.begin(9600);
+  pinMode(DATAQ_pin, OUTPUT);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
@@ -24,8 +25,8 @@ void setup()
 
 void loop()
 {
-    rawreading = (scale.get_units(),1); //scale.get_units() returns a float
-    reading = map(rawreading, 0, 490.5, 0, 255);
-    Serial.println(scale.get_units(), 1); //scale.get_units() returns a float
+    rawreading = scale.get_units(),1; //scale.get_units() returns a float
+    reading = map(rawreading, 0, 490.5, 0, 255); //the 0 to 480.5 is the range of the load cell in newtons
+    //Serial.println(rawreading); //this line is for troubleshooting purposes only
     analogWrite(DATAQ_pin,reading);
 }
